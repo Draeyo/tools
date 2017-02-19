f98a7de312854294585d62c947de8dc784b34f97
Victor <Draeyo@users.noreply.github.com>
2017-02-19 17:58:48 +0100

    check return value

--- ban4.sh
+++ ban4.sh
@@ -1,2 +1,6 @@
 sudo iptables -I INPUT -s $1 -j DROP
-echo "IPv4 adress : $1 successfully banned."
+if [ $? -eq 0 ]; then
+  echo "IPv4 adress : $1 successfully banned."
+else
+  echo "ban failed."
+fi
