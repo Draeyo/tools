498de27c945d54445052b354846e5a47eeb14952
Victor <Draeyo@users.noreply.github.com>
2017-02-19 17:59:37 +0100

    check return value

--- ban.sh
+++ ban.sh
@@ -1,2 +1,6 @@
 sudo ip6tables -I INPUT -s $1 -j DROP
-echo "IPv6 adress : $1 successfully banned."
+if [ $? -eq 0 ]; then
+  echo "IPv6 adress : $1 successfully banned."
+else
+  echo "ban failed."
+fi
