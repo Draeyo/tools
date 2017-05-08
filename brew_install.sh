1adb9351f5d9920f06eb2ed5ccad7ea40a978950
Draeyo <draeyo@gmail.com>
2017-05-07 18:22:26 -0700

    update

--- brew_install.sh
+++ brew_install.sh
@@ -1,1 +1,7 @@
-rm -rf ~/.brew && mkdir ~/.brew && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C ~/.brew && mkdir -p ~/Library/Caches/Homebrew && brew install anything
+rm -rf ~/.brew
+mkdir ~/.brew
+curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C ~/.brew
+mkdir -p ~/Library/Caches/Homebrew
+brew install anything
+brew update
+brew upgrade
