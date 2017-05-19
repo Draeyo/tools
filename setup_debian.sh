2e4b4cf2053768de8ce869b6e944013380c21275
Draeyo <draeyo@gmail.com>
2017-05-19 17:15:31 +0200

    update

--- setup_debian.sh
+++ setup_debian.sh
@@ -1,12 +1,20 @@
+if [$1]
+		name=$1
+else
+		echo "Please specify you main account name as arugument"
+		exit 1
+fi
+
 echo -e "[USER]Switching to root..."
 su
 
 echo -e "[ROOT]Updating..."
 apt-get update
-apt-get upgrade
+apt-get upgrade -y
+apt-get dist-upgrade -y
 
 echo -e "[ROOT]Installing basics..."
-apt-get install -y sudo git vim fail2ban psad htop wget curl ncdu figlet ufw
+apt-get install -y sudo git vim gcc clang wget curl ufw fail2ban psad htop ncdu figlet valgrind
 echo -e "Setting up ufw..."
 ufw enable
 ufw default deny incoming
@@ -18,9 +26,10 @@ ufw allow 443/tcp
 ufw allow 3000
 ufw allow 8080
 ufw status verbose
+sleep 2
 
 echo -e "[ROOT]Installing oh-my-zsh..."
-sh -c "`wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -`"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -`)"
 
 echo -e "[ROOT]Modifying zsh theme..."
 cat ~/.zshrc | sed -e 's/robbyrussell/ys/g' > ~/.zshrc
@@ -32,10 +41,10 @@ echo "node version : `npm -v`" > ~/node_version
 echo "npm version : `npm -v`" >> ~/node_versions
 
 echo -e "[ROOT]Closing root session..."
-exit
+su $name
 
 echo -e "[USER]Installing oh-my-zsh..."
-sh -c "`wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -`"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo -e "[ROOT]Modifying zsh theme..."
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/' ~/.zshrc
@@ -45,3 +54,5 @@ sudo npm install -g pm2
 
 echo -e "[USER]Setting up pm2..."
 sudo pm2 startup
+
+exit
